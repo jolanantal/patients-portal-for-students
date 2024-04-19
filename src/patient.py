611f7e9451f047e7209be26d9f46f29744974a62
up7p2a <jolanantal@student.elte.hu>
2024-04-19 23:33:48 +0200

    make patient class

--- src/patient.py
+++ src/patient.py
@@ -16,3 +16,47 @@ Validation should be done for all of the variables in config and db_config.
 
 There should be a method to commit that patient to the database using the api_controller.
 """
+
+import uuid
+from datetime import datetime, date
+from config import GENDERS,WARD_NUMBERS,ROOM_NUMBERS
+
+
+class Patient:
+    patient_id = ""
+    patient_name = ""
+    patient_age = ""
+    patient_gender = ""
+    patient_checkin = ""
+    patient_checkout = ""
+    patient_ward = ""
+    patient_room = ""
+
+    def __init__(self, Name, Gender, Age):
+        self.patient_name = Name
+        
+        if self.patient_gender not in GENDERS:
+            raise Exception("Gender not a valid gender")
+        
+        self.patient_gender = Gender
+        self.patient_age = Age
+
+        self.patient_id = uuid.uuid4()
+        
+    def checkin(self):
+        self.patient_checkin = datetime.now()
+
+    def checkout(self):
+        self.patient_checkout = datetime.now()
+
+    def set_room(self,room:int):
+        if room not in ROOM_NUMBERS:
+            raise Exception("Room number not valid")
+        
+        self.patient_room = room
+            
+    def set_ward(self,ward):
+        if ward not in WARD_NUMBERS:
+            raise Exception("Room number not valid")
+        
+        self.patient_ward = ward
