34cb06934e1006e675d2be507aa1ee69a00fcbcf
up7p2a <jolanantal@student.elte.hu>
2024-04-19 23:34:08 +0200

    do tasks in api contoller

--- src/api_controller.py
+++ src/api_controller.py
@@ -1,7 +1,8 @@
 """Patient API Controller"""
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from patient_db import PatientDB
+from config import ROOM_NUMBERS,WARD_NUMBERS,GENDERS
 
 
 class PatientAPIController:
@@ -21,7 +22,6 @@ class PatientAPIController:
         self.app.route("/patient/<patient_id>", methods=["PUT"])(self.update_patient)
         self.app.route("/patient/<patient_id>", methods=["DELETE"])(self.delete_patient)
 
-
     """
     TODO:
     Implement the following methods,
@@ -33,20 +33,69 @@ class PatientAPIController:
     """
 
     def create_patient(self):
-        pass
+        try:
+            data = request.json  # Get JSON data from request
+            if not data:
+                return jsonify({"error": "No data provided"}), 400
+
+            if data["patient_gender"] not in GENDERS:
+                return jsonify({"error": "Gender is not valid"}), 400
+
+            if data["patient_room"] not in ROOM_NUMBERS:
+                return jsonify({"error": "Room is not valid"}), 400
+            
+            if data["patient_ward"] not in WARD_NUMBERS:
+                return jsonify({"error": "Room is not valid"}), 400
+        
+            self.patient_db.insert_patient(data)
+
+            return jsonify({"message": "Patient created successfully"}), 200
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
     def get_patients(self):
-
-        return "Halo"
+        patients = self.patient_db.select_all_patients()
+        if patients is not None:
+            return patients
+        
+        return  {[]}
 
     def get_patient(self, patient_id):
-        pass
+        patient = self.patient_db.select_patient(patient_id)
+        if patient is None:
+            return jsonify({"error": "No patient with this id"}), 400
 
-    def update_patient(self, patient_id):
-        pass
+        return patient, 200
+
+    def update_patient(self, patient_id):   
+        try:
+            data = request.json  # Get JSON data from request
+            if not data:
+                return jsonify({"error": "No data provided"}), 400
+
+            if data["patient_gender"] not in GENDERS:
+                return jsonify({"error": "Gender is not valid"}), 400
+
+            if data["patient_room"] not in ROOM_NUMBERS:
+                return jsonify({"error": "Room is not valid"}), 400
+            
+            if data["patient_ward"] not in WARD_NUMBERS:
+                return jsonify({"error": "Room is not valid"}), 400
+        
+            self.patient_db.update_patient(patient_id,data)
+
+            return jsonify({"message": "Patient Updated successfully"}), 200
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
     def delete_patient(self, patient_id):
-        pass
+        deletation = self.patient_db.delete_patient(patient_id)
+        if deletation is None:
+            return jsonify({"error": "Could not delete pationt or no patient with this id"}), 400
+        
+        return jsonify({"message": "Patient Deleted successfully"}), 200
 
     def run(self):
         """
